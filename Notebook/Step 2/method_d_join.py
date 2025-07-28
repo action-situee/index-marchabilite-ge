22077230e11c4f05b341da929196f30ebd729b19
Marques <heloise.marques@free.fr>
2025-07-28 13:16:24 +0200

    Notebooks uploaded

---
+++ Notebook/Step 2/method_d_join.py
@@ -0,0 +1,5 @@
+
+
+def spatial_join_attributes(segments_gdf):
+    # spatial join with other layers, e.g., land use
+    return pd.DataFrame({"segment_id": [...], "land_use": [...], "zoning": [...]})
